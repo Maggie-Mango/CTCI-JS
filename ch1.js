c5c9ef220e751a32b135965e52c1b596fc1fe0b4
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-03-29 11:13:04 -0400

    complete urlify

--- ch1.js
+++ ch1.js
@@ -26,8 +26,34 @@ const isPermutation = (str1, str2) => {
 //time complexity o(logn) - they inrease in running time depending on input size,
 //but not as quickly as linear time
 
+//1.3 URLify
+const urlify = (str, length) => {
+  const urlify = (str, length) => {
+    str = str.split('')
+    let whiteSp = 0
+    for (let i = 0; i < length; i++) {
+      str[i] === ' ' ? whiteSp += 1 : whiteSp += 0
+    }
+    let index = length + (whiteSp * 2)
+
+    for (let i = length - 1; i > -1; i--) {
+     // console.log(str[i])
+     if (str[i] === ' ') {
+       str[index - 1] = 2
+       str[index - 2] = 0
+       str[index - 3] = '%'
+       index -=3
+     } else {
+       str[index - 1] = str[i]
+       index -= 1
+     }
+    }
+    return str.join('')
+  }
+}
 
 module.exports = {
   isUnique,
-  isPermutation
+  isPermutation,
+  urlify
 }
