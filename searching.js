2a94e2e2a463241259b8c759fe7a6dc9fca30dae
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-06-24 15:34:50 -0400

    complete BST add

--- searching.js
+++ searching.js
@@ -30,6 +30,34 @@ class Tree {
     this.root = null;
   }
 
+  add(value) {
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while(true) {
+        if (current.value > value) {
+          //go left
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          //go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    //return the tree
+    return this;
+  }
 
 
 }
@@ -40,5 +68,12 @@ class Node {
     this.value = value;
     this.left = null;
     this.right = null;
+  }
+
+  serialize() {
+    const ans = { value: this.value};
+    ans.left = this.left === null ? null : this.left.serialize();
+    ans.right = this.right === null ? null : this.right.serialize();
+    return ans;
   }
 }
