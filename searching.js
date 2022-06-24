06dfef296eaeece591accfb92640ba134cc4d3b9
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-06-24 15:28:20 -0400

    starting bst add

--- searching.js
+++ searching.js
@@ -23,3 +23,22 @@ function linearSearch(id, array) {
   }
   return void 0;
 }
+
+//Binary search tree
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+
+
+}
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
