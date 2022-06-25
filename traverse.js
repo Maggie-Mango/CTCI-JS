a7024766dae48993ff9497ad2e7946d699614d39
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-06-25 12:41:25 -0400

    add traversals

---
+++ traverse.js
@@ -0,0 +1,69 @@
+const preorderTraverse = (node, array) {
+  if (!node) return array;
+  array.push(node.value);
+  array = preorderTraverse(node.left, array);
+  array = preorderTraverse(node.right, array);
+  return array;
+}
+
+const inorderTraverse = (node, array) {
+  if (!node) return array;
+  array = inorderTraverse(node.left, array);
+  array.push(node.value);
+  array = inorderTraverse(node.right, array);
+  return array;
+}
+
+cost postorderTraverse = (node, array) {
+  if (!node) return array;
+  array = postorderTraverse(node.left, array);
+  array = postorderTraverse(node.right, array);
+  array.push(node.value);
+  return array;
+}
+
+/* tree
+const tree = {
+    value: 8,
+    left: {
+      value: 4,
+      left: {
+        value: 3,
+        left: {
+          value: 2,
+          left: null,
+          right: null
+        },
+        right: null
+      },
+      right: {
+        value: 5,
+        left: null,
+        right: {
+          value: 7,
+          left: {
+            value: 6,
+            left: null,
+            right: null
+          }
+        }
+      }
+    },
+    right: {
+      value: 12,
+      left: {
+        value: 10,
+        left: {
+          value: 9,
+          left: null,
+          right: null
+        },
+        right: {
+          value: 11,
+          left: null,
+          right: null
+        }
+      }
+    }
+  };
+  */
