f1356b49d3e5c45259ce3fbf9c5ba8a57c16a89f
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-01 11:50:12 -0400

    add recursive breadthFirst

--- traverse.js
+++ traverse.js
@@ -75,5 +75,9 @@ const tree = {
     if (!queue.length) return array;
 
     const node = queue.shift();
-    array.push(node.value)
+    array.push(node.value);
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+    return breadthFirstTraverse(queue, array);
   };
