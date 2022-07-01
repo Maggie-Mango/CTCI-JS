631e105ad3e7bc341eff4ed4488d21cca2525dca
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-01 15:08:41 -0400

    add bfs2

--- traverse.js
+++ traverse.js
@@ -81,3 +81,16 @@ const tree = {
     if (node.right) queue.push(node.right);
     return breadthFirstTraverse(queue, array);
   };
+
+  //breadthFirst again
+  const breadthFirstTraverse2 = (queue, array) => {
+
+    while (queue.length) {
+      const node = queue.shift();
+      array.push(node.value);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return array;
+  }
