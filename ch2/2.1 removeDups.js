1b2ef01eae300a145fd04b302fd045bbb9eff1ad
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-09 20:07:54 -0400

    complete 2.1

--- ch2/2.1 removeDups.js
+++ ch2/2.1 removeDups.js
@@ -2,17 +2,24 @@ const LinkedList = require("../util/linkedList");
 
 //Remove dup fron an unsorted linkedlist
 const removeDups = (lList) => {
-  let visited = {}
-  let curr = lList.head;
-  while (curr) {
-    if (visited[curr.val]) {
-      curr = curr.next;
+  let visited = {};
+  let newSize = 0;
+  let current = lList.head;
+  //if the first value is unique, start the counter at 1, otherwise it will be accounted for
+  if (current.val !== current.next.val) {
+    newSize++;
+  }
+  while (current.next) {
+    if (visited[current.next.val]) {
+      current.next = current.next.next;
     } else {
-      visited[curr.val] = true;
-      curr = curr.next;
+      visited[current.next.val] = true;
+      current = current.next;
     }
   }
-  console.log(visited)
+  newSize += Object.keys(visited).length;
+  lList.size = newSize;
+  return lList.printList();
 }
 
 
