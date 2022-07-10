372b4f51cf5f7054f71bf5e9f3d33eda5a1e6721
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-10 12:45:14 -0400

    complete 2.2

--- ch2/2.2 returnKth.js
+++ ch2/2.2 returnKth.js
@@ -4,8 +4,24 @@ const LinkedList = require("../util/linkedList");
 //Find the kth to the last element of a singly linked list
 
 const kthElement = (k, linkedList) => {
-  let slow = linkedList.head;
-  let fast = linkedList.head;
+  let curr = linkedList;
+  curr.next = linkedList.head;
+  let slow = curr;
+  let fast = curr;
+  let counter = 0;
+
+  while (k  > 0) {
+    fast = fast.next;
+    k--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow.next;
+
 }
 
 
@@ -17,4 +33,4 @@ for (let e of [1, 2, 3, 4, 5, 6, 7, 8, 8]) {
   list.add(e);
 }
 
-kthElement(3, list);
+console.log(kthElement(3, list));
