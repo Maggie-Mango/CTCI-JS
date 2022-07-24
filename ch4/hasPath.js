bc7233afadb515aad1019ba5162e2a3a541dfcb4
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-24 18:19:50 -0400

    add hasPath dfs

---
+++ ch4/hasPath.js
@@ -0,0 +1,29 @@
+const hasPath = (graph, src, dst) => {
+  //depth first
+  if (src === dst) return true
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst) === true) {
+       return true;
+    }
+  }
+  return false;
+};
+
+module.exports = {
+  hasPath,
+};
+
+/*
+
+const graph = {
+  f: ['g', 'i'],
+  g: ['h'],
+  h: [],
+  i: ['g', 'k'],
+  j: ['i'],
+  k: []
+};
+
+console.log(hasPath(graph, 'f', 'k')); // true
+*/
