b39172c134e0af3920dec24f223c3b91e9f140af
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-24 18:26:59 -0400

    added bfs solution to hasPath

--- ch4/hasPath.js
+++ ch4/hasPath.js
@@ -9,6 +9,25 @@ const hasPath = (graph, src, dst) => {
   }
   return false;
 };
+//breadth first
+/*
+const hasPath = (graph, src, dst) => {
+  const queue = [ src ];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current === dst) return true;
+
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
+  }
+  return false;
+};
+*/
+
+module.exports = {
+  hasPath,
+};
 
 module.exports = {
   hasPath,
