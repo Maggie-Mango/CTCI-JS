497ffb38fceeea3e2217dea094bbc4506e73e9b0
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-27 17:18:56 -0400

    add treeincludes bfs and dfs

---
+++ ch4/treeIncludes.js
@@ -0,0 +1,12 @@
+const treeIncludes = (root, target) => {
+  if (root === null) return false;
+  const queue = [ root ];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current.val === target) return true;
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right)
+  }
+  return false;
+};
