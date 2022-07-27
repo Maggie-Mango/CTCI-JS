916e86139119990e88f38e8c7b9c3fef7fbe5134
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-27 14:07:02 -0400

    add bfs

---
+++ ch4/bfs.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const bfs = (root) => {
+  if (root === null) return [];
+  let result = [];
+  const queue = [ root ];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    queue.push(current.val);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return result;
+}
