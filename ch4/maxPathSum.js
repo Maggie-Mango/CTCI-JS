0713396ea54f9c33d7f60f03dfc23c37df1f4c4b
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-07-28 16:58:48 -0400

    add maxPathSum

---
+++ ch4/maxPathSum.js
@@ -0,0 +1,10 @@
+const maxPathSum = (root) => {
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  const maxChild = Math.max(maxPathSum(root.left), maxPathSum(root.right))
+  return root.val + maxChild
+};
+
+module.exports = {
+  maxPathSum,
+};
