b68b1032c861c66382c4363d219963b3214c76c9
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-08-03 22:22:11 -0400

    a little closer for treelevel

--- Blind75/Trees/binaryTreeLevelOrderTraversal.js
+++ Blind75/Trees/binaryTreeLevelOrderTraversal.js
@@ -6,13 +6,17 @@ var levelOrder = function(root) {
   //breadth first
   let levels = [ ];
   let queue = [ root ];
+
   while (queue.length > 0) {
-    let level = [];
-    const current = queue.shift();
-    level.push(current.val);
-    if (current.left) queue.push(current.left);
-    if (current.right) queue.push(current.right);
-    levels.push(level);
+    let row = [ ];
+    for (let i = 0; i < queue.length; i++) {
+        let current = queue.shift();
+        row.push(current.val);
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+    }
+
+    levels.push(row);
   }
   return levels;
 };
