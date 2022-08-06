3bf05d3126aed1ab2d52a5d39017b95e65fe352a
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-08-06 14:36:32 -0400

    add kthSmallest solution

---
+++ Blind75/Trees/kthSmallest.js
@@ -0,0 +1,18 @@
+
+//O(N) - traversing through tree once
+//O(N) - adding N amount to array
+
+const kthSmallest = (root, k) => {
+  //global results
+  let res = [];
+  dfs(root, res);
+  return res[k - 1];
+}
+
+const dfs = (node, res) => {
+  //inorder to get the nodes in order
+  if (!node) return;
+  if (node.left) dfs(node.left);
+  res.push(node.val);
+  if (node.right) dfs(node.right);
+}
