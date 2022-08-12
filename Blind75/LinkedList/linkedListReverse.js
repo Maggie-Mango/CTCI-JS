6a24341fc00c270b35823a817ad5e31963a5b12c
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-08-12 18:20:08 -0400

    add reverse linkedlist

---
+++ Blind75/LinkedList/linkedListReverse.js
@@ -0,0 +1,22 @@
+ //O(N) time
+ //O(N) space
+
+const reverseList = (head, prev = null) => {
+  if (head === null) return prev;
+  const next = head.next;
+  head.next = prev;
+  return reverseList(next, head)
+};
+
+
+const reverseList = (head) => {
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
