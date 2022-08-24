f7c1bb6cf7abae81c1ad0e18b35da66ccbcf4563
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-08-23 21:43:42 -0400

    3sum leetcode

--- Blind75/arrays/threeSum.js
+++ Blind75/arrays/threeSum.js
@@ -19,3 +19,32 @@ function threeNumberSum(array, targetSum) {
   }
   return triplet;
 }
+
+//edited to watch out for dups
+var threeSum = function(nums) {
+  nums.sort((a, b) => a - b);
+  let target = 0;
+  let triplet = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+      let left = i + 1;
+      let right = nums.length - 1;
+      while (left < right) {
+          const current = nums[i] + nums[left] + nums[right];
+
+          if (current === target) {
+              triplet.push([nums[i], nums[left], nums[right]]);
+              while (nums[left + 1] === nums[left]) left++;
+              while (nums[right - 1] === nums[right]) right--;
+              left++;
+              right--;
+          } else if (current < target) {
+              left++;
+          } else if (current > target) {
+              right--;
+          }
+
+      }
+      while (nums[i + 1] === nums[i]) i++;
+  }
+  return triplet;
+};
