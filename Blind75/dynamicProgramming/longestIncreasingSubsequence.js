feb75e5ed1258f35e7bb3d9b99bebc4fa75a371e
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-09-04 20:27:50 -0400

    optimized lis bst

--- Blind75/dynamicProgramming/longestIncreasingSubsequence.js
+++ Blind75/dynamicProgramming/longestIncreasingSubsequence.js
@@ -17,3 +17,52 @@ var lengthOfLIS = function(nums) {
      }
      return longest
  };
+
+
+
+ //more optimized solution
+ /*
+ Algorithm
+
+Initialize an array sub which contains the first element of nums.
+
+Iterate through the input, starting from the second element. For each element num:
+
+If num is greater than any element in sub, then add num to sub.
+Otherwise, perform a binary search in sub to find the smallest element that is greater than or equal to num. Replace that element with num.
+Return the length of sub.
+
+t = O(n * log(n))
+space = o(n)
+*/
+
+
+
+ var lengthOfLIS = function(nums) {
+  let sub = [ nums[0] ];
+  for (let i = 1; i <  nums.length; i++) {
+      let num = nums[i];
+      if (num > sub[sub.size - 1]) {
+          sub.push(num)
+      } else {
+          const j = binarySearch(sub, num);
+          sub[j] = num;
+      }
+  }
+  return sub.length;
+};
+
+const binarySearch = (arr, curr) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (arr[mid] < curr) {
+          left = mid + 1;
+      } else {
+          right = mid - 1;
+      }
+  }
+  return left;
+}
