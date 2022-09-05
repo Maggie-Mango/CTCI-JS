0e5eee11ff841ce4cea880f434e8eb671d7d0419
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-09-05 15:14:43 -0400

    lcs (need to practice)

--- Blind75/dynamicProgramming/longestCommonSubsequence.js
+++ Blind75/dynamicProgramming/longestCommonSubsequence.js
@@ -5,18 +5,19 @@ var longestCommonSubsequence = function(text1, text2) {
   for (let i = 0; i <= text1.length; i++) {
       dp.push(new Array(text2.length + 1).fill(0))
   }
-  console.log(dp)
   for (let i = 1; i < dp.length; i++) {
-      for (let j = 1; j < dp[0].length; j++) {
-          if(text1[i - 1] === text2[j - 1]) {
-              console.log('here ', dp[i - 1][j - 1])
-              dp[i][j] = dp[i - 1][j - 1] + 1;
-          } else {
-              dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      for (let j = 1; j < dp[i].length; j++) {
+          if(text1[i - 1] === text2[j - 1]) { //bc were setting table to be length 1 more, need i - 1
+              dp[i][j] = dp[i - 1][j - 1] + 1; //set the previous one to +1
+          } else { //get the max of the above char or left char
+              let aboveChar = dp[i - 1][j];
+              let leftChar = dp[i][j - 1]
+              dp[i][j] = Math.max(aboveChar, leftChar)
           }
           max = Math.max(max, dp[i][j])
       }
   }
-  console.log(dp)
+
   return max;
 };
+//helpful https://www.youtube.com/watch?v=bocaOl7hvTY
